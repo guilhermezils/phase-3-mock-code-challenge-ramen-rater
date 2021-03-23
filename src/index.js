4416a9a2974d86bb36e27fc50a603e542c72e751
dapeshcode <ampeccia@gmail.com>
2021-03-23 14:11:10 -0500

    3rd deliverable

--- src/index.js
+++ src/index.js
@@ -18,10 +18,10 @@ function renderOneRamen (element){
 //event listener for a click on a particular img
 //will need a fetch request for a page for that image
 //will need to render that ramen with its attr
-
+const form =  document.querySelector('form#ramen-rating')
 parentDiv.addEventListener('click', (event)=> {
     const ramenDetail = document.querySelector('div#ramen-detail')
-    const form =  ramenDetail.nextElementSibling
+    const form =  document.querySelector('form#ramen-rating')
     let rating = form[0] 
     let comment = form[1]
     
@@ -33,16 +33,65 @@ parentDiv.addEventListener('click', (event)=> {
         const name = img.nextElementSibling
         const restaurant = name.nextElementSibling
 
-fetch (ramenUrl + `/${id}`)
-.then(response => response.json())
-.then(ramenObj => {
-name.textContent = ramenObj.name 
-restaurant.textContent = ramenObj.restaurant
-rating.value = ramenObj.rating
-comment.value = ramenObj.comment
-})
+        fetch (ramenUrl + `/${id}`)
+        .then(response => response.json())
+        .then(ramenObj => {
+        name.textContent = ramenObj.name 
+        restaurant.textContent = ramenObj.restaurant
+        rating.value = ramenObj.rating
+        comment.value = ramenObj.comment
+        })
 
+        form.addEventListener(`submit`, (event) => {
+            event.preventDefault()
+            // console.log(event.target)
+            // const id = event.target.closest('img').dataset
+            const rating = form[0].value 
+            const comment = form[1].value
+        
+            fetch(ramenUrl + `/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    rating,
+                    comment
+                })
+            })
+            .then(response => response.json())
+            .then(ramenObj => console.log(ramenObj))
+        
+        })
 
     }
 
+
+   
+
 })
+
+
+// form.addEventListener(`submit`, (event) => {
+//     event.preventDefault()
+//     // console.log(event.target)
+//     const id = event.target.closest('img').dataset
+//     const rating = form[0].value 
+//     const comment = form[1].value
+
+//     fetch(ramenUrl + `/${id}`, {
+//         method: 'PATCH',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             rating,
+//             comment
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(ramenObj => console.log(ramenObj))
+
+// })
